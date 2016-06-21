a655b657583a78cfd3d700b8706bf05700690238
maximest-pierre <maxime.st-pierre.3@ens.etsmtl.ca>
2016-06-21 17:47:42 -0400

    Add test for the member model

--- django/cedille/member/tests.py
+++ django/cedille/member/tests.py
@@ -1,3 +1,36 @@
 from django.test import TestCase
+from member.models import Member
 
-# Create your tests here.
+
+class MemberTestCase(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        member = Member.objects.create(
+            firstname='Club', lastname='Cedille', status='A',
+            biography="Club Cedille", title="Club"
+        )
+
+    def testFirstName(self):
+        """A member must have a First Name"""
+        member = Member.objects.get(firstname="Club")
+        self.assertEqual(member.firstname, 'Club')
+
+    def testLastName(self):
+        """A member must have a Last Name"""
+        member = Member.objects.get(firstname="Club")
+        self.assertEqual(member.lastname, 'Cedille')
+
+    def testStatus(self):
+        """A member must have a status"""
+        member = Member.objects.get(firstname="Club")
+        self.assertEqual(member.status, 'A')
+
+    def testBiography(self):
+        """A member must have a biography"""
+        member = Member.objects.get(firstname="Club")
+        self.assertEqual(member.biography, "Club Cedille")
+
+    def testTitle(self):
+        """A member must have a title"""
+        member = Member.objects.get(firstname="Club")
+        self.assertEqual(member.title, "Club")
