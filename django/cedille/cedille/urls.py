25b43f06031a00db285546214ba0657f483a2898
maximest-pierre <maxime.st-pierre.3@ens.etsmtl.ca>
2016-07-01 13:29:22 -0400

    Add the template for the member application

--- django/cedille/cedille/urls.py
+++ django/cedille/cedille/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf import settings
 
 
 from page.views import home
@@ -26,4 +28,4 @@ urlpatterns = [
     url(r'^partners/', include('partners.urls', namespace="partners")),
     url(r'^$', home, name='page')
 
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- django/cedille/member/urls.py
+++ django/cedille/member/urls.py
@@ -1,3 +1,8 @@
-urlpatterns = [
+from django.conf.urls import url
+from member.views import MemberList
+from django.conf.urls.static import static
 
+
+urlpatterns = [
+    url(r'^view/', MemberList.as_view(), name='member_list')
 ]
