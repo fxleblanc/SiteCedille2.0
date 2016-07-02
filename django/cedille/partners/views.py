e9ccc01d9e3f53c6588f325ae79f8fdd0df58bf3
maximest-pierre <maxime.st-pierre.3@ens.etsmtl.ca>
2016-07-01 22:34:19 -0400

    add route and basic template for partners

--- django/cedille/partners/views.py
+++ django/cedille/partners/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.views import generic
+from partners.models import Partner
 
-# Create your views here.
+
+class PartnerList(generic.ListView):
+    model = Partner
+    template_name = 'view_partners.html'
+    context_object_name = "partners"
+
+    def get_context_data(self, **kwargs):
+        context = super(PartnerList, self).get_context_data(**kwargs)
+
+        context['myVariableOfContext'] = 0
+
+        return context
