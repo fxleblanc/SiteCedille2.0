2fddd795aca680e558d8e9447da51c42e4215974
maximest-pierre <maxime.st-pierre.3@ens.etsmtl.ca>
2016-07-04 20:12:50 -0400

    Add test for project

--- django/cedille/projects/tests.py
+++ django/cedille/projects/tests.py
@@ -1,3 +1,31 @@
 from django.test import TestCase
+from projects.models import Project
 
-# Create your tests here.
+
+class PartnerTestCase(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        project = Project.objects.create(
+            name="ClubCedille", description="123456", github="123456",
+            status="A"
+        )
+
+    def testProjectName(self):
+        """Test a project name"""
+        project = Project.objects.get(name="ClubCedille")
+        self.assertEqual(project.name, "ClubCedille")
+
+    def testProjectDescription(self):
+        """Test a project description"""
+        project = Project.objects.get(name="ClubCedille")
+        self.assertEqual(project.description, "123456")
+
+    def testProjectGithub(self):
+        """Test a project github"""
+        project = Project.objects.get(name="ClubCedille")
+        self.assertEqual(project.github, "123456")
+
+    def testProjectStatus(self):
+        """Test a project Status"""
+        project = Project.objects.get(name="ClubCedille")
+        self.assertEqual(project.status, "A")
