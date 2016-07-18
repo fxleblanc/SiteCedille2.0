05f7005996dd649cf8703355f65deb4475993ea2
maximest-pierre <maxime.st-pierre.3@ens.etsmtl.ca>
2016-07-18 10:49:10 -0400

    Add model for the blog module

--- django/cedille/blog/models.py
+++ django/cedille/blog/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
+from django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your models here.
+
+class Blog(models.Model):
+
+    class Meta:
+        verbose_name_plural = _('members')
+
+    text = models.TextField(
+        verbose_name=_('Notes')
+    )
+
+    author = models.ForeignKey(
+        User,
+        verbose_name=_('Author'),
+        related_name='Notes'
+        )
+
+    date = models.DateField(
+        verbose_name=_('Date'),
+        default=timezone.now,
+    )
